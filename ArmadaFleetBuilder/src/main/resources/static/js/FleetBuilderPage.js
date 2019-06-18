3044f370511a3692bfdbab76887d261c2ea1bf92
cowking96 <kakson96@yahoo.com>
2019-06-18 17:51:17 -0400

    Adds dialog box functionality to advanced search

--- ArmadaFleetBuilder/src/main/resources/static/js/FleetBuilderPage.js
+++ ArmadaFleetBuilder/src/main/resources/static/js/FleetBuilderPage.js
@@ -5,11 +5,103 @@ FleetBuilderPage = {
         this.baseUrl = generatedURL;
 
         //init widgets
-        $( ".searchButton" ).button();
-        $( ".advancedSearchButton" ).button();
-        $( ".radioButtons" ).controlgroup();
-        $( ".fleetAccordion" ).accordion();
+        $( "#searchButton" ).button();
+        $( "#advancedSearchButton" ).button();
+        $( "#closeButton" ).button();
+        $( "#radioSearchUnits" ).checkboxradio();
+        $( "#radioSearchCards" ).checkboxradio();
+        $( "#fleetAccordion" ).accordion();
+        $( "#includeNeutralsCheckboxUnits" ).checkboxradio();
+        $( "#includeNeutralsCheckboxCards" ).checkboxradio();
+        $( "#searchButtonAdvancedUnits" ).button();
+        $( "#searchButtonAdvancedCards" ).button();
+        $( "#closeButtonAdvancedUnits" ).button();
+        $( "#closeButtonAdvancedCards" ).button();
 
 
-    }
+
+
+        //binds
+        this.bindSearchSubmit();
+        this.bindAdvancedSearch();
+        this.bindCloseButton();
+
+    },
+
+    onSearch: function(){
+
+        var unitRadioChecked = $("#radioSearchUnits").is(":checked");
+        alert("value is: " + unitRadioChecked );
+        if(unitRadioChecked){
+            $( "#searchResultsPanel" ).html("searched for units")
+        }
+
+        else{
+            $( "#searchResultsPanel" ).html("searched for cards")
+        }
+    },
+
+    bindSearchSubmit: function() {
+
+            $("#searchButton").click(function() {
+                FleetBuilderPage.onSearch();
+            });
+
+            $("#searchButtonAdvancedUnits").click(function() {
+                FleetBuilderPage.onSearch();
+            });
+
+            $("#searchButtonAdvancedCards").click(function() {
+                FleetBuilderPage.onSearch();
+            });
+    },
+
+
+    bindAdvancedSearch: function(){
+        $("#advancedSearchButton").click(function() {
+            FleetBuilderPage.onAdvancedSearch();
+        });
+
+    },
+
+    bindCloseButton: function(){
+            $("#closeButtonAdvancedUnits").click(function() {
+                FleetBuilderPage.onClose();
+            });
+
+            $("#closeButtonAdvancedCards").click(function() {
+                FleetBuilderPage.onClose();
+            });
+
+    },
+
+    onClose: function(){
+        var unitRadioChecked = $("#radioSearchUnits").is(":checked");
+        if(unitRadioChecked){
+            $("#advancedSearchFormUnits").dialog("close");
+        }
+        else{
+            $("#advancedSearchFormCards").dialog("close");
+        }
+        
+        $( "#radioSearchUnits" ).checkboxradio("enable");
+        $( "#radioSearchCards" ).checkboxradio("enable");
+
+    },
+
+    onAdvancedSearch: function(){
+        $( "#radioSearchUnits" ).checkboxradio("disable");
+        $( "#radioSearchCards" ).checkboxradio("disable");
+        var unitRadioChecked = $("#radioSearchUnits").is(":checked");
+            if(unitRadioChecked){
+                $("#advancedSearchFormUnits").dialog({dialogClass:"no-close"});
+
+
+            }
+            else{
+                $("#advancedSearchFormCards").dialog({dialogClass:"no-close"});
+            }
+
+    },
 }
+
